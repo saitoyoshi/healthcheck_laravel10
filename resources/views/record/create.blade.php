06cd7af6d1ef58b84e88c46fbacaa6fa128c6667
saitoyoshi <mitti180@gmail.com>
2023-08-15 16:28:04 +0900

    oldヘルパー導入

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -12,28 +12,28 @@
     @csrf
     <div>
     <label for="">体調</label>
-    <input type="number" name="physical_condition" id="">
+    <input type="number" name="physical_condition" id="" value="{{ old('physical_condition') }}">
 </div>
 <div>
     <label for="">気分</label>
-    <input type="number" name="mood_state" id="">
+    <input type="number" name="mood_state" id="" value="{{ old('mood_state') }}">
 </div>
 <div>
     <label for="">肩こり</label>
-    <input type="number" name="back_pain" id="">
+    <input type="number" name="back_pain" id="" value="{{ old('back_pain') }}">
 </div>
 <div>
     <label for="">眼精疲労</label>
-    <input type="number" name="eyestrain" id="">
+    <input type="number" name="eyestrain" id="" value="{{ old('eyestrain') }}">
 
 </div>
 <div>
     <label for="">頭痛</label>
-    <input type="number" name="headache" id="">
+    <input type="number" name="headache" id="" value="{{ old('headache') }}">
 </div>
 <div>
     <label for="">記録日時</label>
-    <input type="date" name="recording_date" id="">
+    <input type="date" name="recording_date" id="" value="{{ old('recording_date') }}">
 </div>
 <button>登録</button>
 </form>
