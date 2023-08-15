c18d60caddeabb120fcabce50e4f0900d769ab76
saitoyoshi <mitti180@gmail.com>
2023-08-15 12:28:44 +0900

    renamed:    app/Http/Controllers/IndexController.php -> app/Http/Controllers/RecordController.php

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Record;
 use Illuminate\Http\Request;
 
-class IndexController extends Controller
+class RecordController extends Controller
 {
     public function index() {
         $records = Record::all();
