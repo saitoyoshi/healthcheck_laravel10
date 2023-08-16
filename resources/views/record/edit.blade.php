a70e88d9b2301ecbed89e28220d24f260ba5075d
saitoyoshi <mitti180@gmail.com>
2023-08-16 14:31:33 +0900

    更新画面のフォーム、ボタンにデザイン適応

--- resources/views/record/edit.blade.php
+++ resources/views/record/edit.blade.php
@@ -10,34 +10,34 @@
   <form action="{{ route('record.update', $record) }}" method="post">
     @csrf
     @method('put')
-    <div>
-    <label for="">体調</label>
-    <input type="number" name="physical_condition" id="" value="{{ old('physical_condition', $record->physical_condition) }}">
+    <div class="mb-3">
+    <label for="" class="form-label">体調</label>
+    <input class="form-control" type="number" name="physical_condition" id="" value="{{ old('physical_condition', $record->physical_condition) }}">
 </div>
-<div>
-    <label for="">気分</label>
-    <input type="number" name="mood_state" id="" value="{{ old('mood_state',$record->mood_state) }}">
+<div class="mb-3">
+    <label for="" class="form-label">気分</label>
+    <input class="form-control" type="number" name="mood_state" id="" value="{{ old('mood_state',$record->mood_state) }}">
 </div>
-<div>
-    <label for="">肩こり</label>
-    <input type="number" name="back_pain" id="" value="{{ old('back_pain', $record->back_pain) }}">
+<div class="mb-3">
+    <label for="" class="form-label">肩こり</label>
+    <input class="form-control" type="number" name="back_pain" id="" value="{{ old('back_pain', $record->back_pain) }}">
 </div>
-<div>
-    <label for="">眼精疲労</label>
-    <input type="number" name="eyestrain" id="" value="{{ old('eyestrain', $record->eyestrain) }}">
+<div class="mb-3">
+    <label for="" class="form-label">眼精疲労</label>
+    <input class="form-control" type="number" name="eyestrain" id="" value="{{ old('eyestrain', $record->eyestrain) }}">
 
 </div>
-<div>
-    <label for="">頭痛</label>
-    <input type="number" name="headache" id="" value="{{ old('headache', $record->headache) }}">
+<div class="mb-3">
+    <label for="" class="form-label">頭痛</label>
+    <input class="form-control" type="number" name="headache" id="" value="{{ old('headache', $record->headache) }}">
 </div>
-<div>
-    <label for="">記録日時</label>
-    <input type="date" name="recording_date" id="" value="{{ old('recording_date', $record->recording_date) }}">
+<div class="mb-3">
+    <label for="" class="form-label">記録日時</label>
+    <input class="form-control" type="date" name="recording_date" id="" value="{{ old('recording_date', $record->recording_date) }}">
 </div>
-<div>
-    <textarea name="comment" id="" cols="30" rows="5">{{ old('comment', $record->comment) }}</textarea>
+<div class="mb-3">
+    <textarea class="form-control" name="comment" id="" cols="30" rows="5">{{ old('comment', $record->comment) }}</textarea>
 </div>
-<button>更新</button>
+<button class="btn btn-primary">更新</button>
 </form>
 </x-layout.layout>
