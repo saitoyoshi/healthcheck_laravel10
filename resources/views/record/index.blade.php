8351b2af1923dc3f5ee60aaba618c4a0521e60c1
saitoyoshi <mitti180@gmail.com>
2023-08-16 09:19:22 +0900

    HTML構造変更、記録の一つ一つに作成見出しをつける

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -1,27 +1,33 @@
 <x-layout.layout>
     <x-slot:title>記録一覧</x-slot:title>
 
-  <h2>記録一覧画面</h2>
-  <x-message-function></x-message-function>
-  @if(session('message'))
-    <p style="color:blue">{{ session('message') }}</p>
-  @endif
-  <a href="{{ route('record.create') }}">追加</a>
-  @foreach($records as $record)
-    <ul>
-        <a href="{{ route('record.edit', $record) }}"><button>更新</button></a>
-        <form action="{{ route('record.destroy', $record) }}" method="post">
-            @csrf
-            @method('delete')
-            <button onclick="return confirm('本当に削除しますか');">削除</button>
-
-        </form>
-        <li>体調: {{ message($record->physical_condition) }}</li>
-        <li>気分: {{ message($record->mood_state) }}</li>
-        <li>肩こり: {{ message($record->back_pain) }}</li>
-        <li>目の疲れ: {{ message($record->eyestrain) }}</li>
-        <li>頭痛: {{ message($record->headache) }}</li>
-        <li>記録日時: {{ date('Y年n月j日', strtotime($record->recording_date)) }}</li>
-    </ul>
-  @endforeach
+    <h2>記録一覧画面</h2>
+    <x-message-function></x-message-function>
+    @if (session('message'))
+        <p style="color:blue">{{ session('message') }}</p>
+    @endif
+    <div class="py-3">
+        <a href="{{ route('record.create') }}">追加</a>
+    </div>
+    @foreach ($records as $record)
+    <ul class="mb-4">
+            <div class="row mb-2">
+            <h5 class="col-auto">作成日時: {{ date('Y/n/j', strtotime($record->updated_at)) }}</h5>
+                <div class="col-auto">
+                    <a href="{{ route('record.edit', $record) }}"><button class="">更新</button></a>
+                </div>
+                <form class="col-auto" action="{{ route('record.destroy', $record) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button class="d-block" onclick="return confirm('本当に削除しますか');">削除</button>
+                </form>
+            </div>
+            <li>体調: {{ message($record->physical_condition) }}</li>
+            <li>気分: {{ message($record->mood_state) }}</li>
+            <li>肩こり: {{ message($record->back_pain) }}</li>
+            <li>目の疲れ: {{ message($record->eyestrain) }}</li>
+            <li>頭痛: {{ message($record->headache) }}</li>
+            <li>記録日時: {{ date('Y年n月j日', strtotime($record->recording_date)) }}</li>
+        </ul>
+    @endforeach
 </x-layout.layout>
